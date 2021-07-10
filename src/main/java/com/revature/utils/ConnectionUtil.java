1cdb6375c48beed222cebf2b44009dee22ac91d9
Chani Kinsler (CK Tha Poet) <kinsler_chani@yahoo.com>
2021-07-10 19:48:24 -0400

    Made class adjustments and added dependencies and utilities and resources
    Commented out ConnectionUtil class might need if I decide to do JDBC instead of hibernate.
    
    added hibernate dependency to Pom.XML so I can use this in my project
    
    added HibernateUtil class with configurations for sessions
    
    because I didnt have one had to create a resource folder to add hibernate.cfg.xml  so I can configure hibernate connection with my credentials  I will map classes later and adjust hbm2ddl.auto after table creation.

--- src/main/java/com/revature/utils/ConnectionUtil.java
+++ src/main/java/com/revature/utils/ConnectionUtil.java
@@ -1,55 +1,56 @@
-package com.revature.utils;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class ConnectionUtil {
-	//this is for testing the connection
-		public static void main(String[] args) {
-			//here we're just testing whether our connection (from the ConnectionUtil Class) is successful
-			//remember - the getConnection() method will return a Connection object if you successfully reach the database
-			try(Connection conn = ConnectionUtil.getConnection()) {
-				System.out.println("connection successful");
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-	}
-		//This Class contains the logic that gets a connection to our database
-		//It will have a method called getConnection() that will return a Connection object
-		//We will need these Connection object in our DAO layer to interact with our database
-			
-			//a method called getConnection that returns a Connection object
-			public static Connection getConnection() throws SQLException {
-				
-				//For compatibility with other technologies/frameworks, we'll need to register our Driver
-				//This process makes the application aware of what Driver class (what SQL dialect) we're using
-				try {
-					Class.forName("org.postgresql.Driver"); //try to find and return the postgresql Driver Class
-				} catch (ClassNotFoundException e) {
-					System.out.println("Class wasn't found :(");
-					e.printStackTrace(); //prints the exception message to the console if we can't find the postgresql class
-				}
-				
+// this will only be used if I decide to use Jave Database Connectivity instead of Hibernate
+//package com.revature.utils;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//public class ConnectionUtil {
+//	//this is for testing the connection
+//		public static void main(String[] args) {
+//			//here we're just testing whether our connection (from the ConnectionUtil Class) is successful
+//			//remember - the getConnection() method will return a Connection object if you successfully reach the database
+//			try(Connection conn = ConnectionUtil.getConnection()) {
+//				System.out.println("connection successful");
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//			}
+//			
+//	}
+//		//This Class contains the logic that gets a connection to our database
+//		//It will have a method called getConnection() that will return a Connection object
+//		//We will need these Connection object in our DAO layer to interact with our database
+//			
+//			//a method called getConnection that returns a Connection object
+//			public static Connection getConnection() throws SQLException {
+//				
+//				//For compatibility with other technologies/frameworks, we'll need to register our Driver
+//				//This process makes the application aware of what Driver class (what SQL dialect) we're using
+//				try {
+//					Class.forName("org.postgresql.Driver"); //try to find and return the postgresql Driver Class
+//				} catch (ClassNotFoundException e) {
+//					System.out.println("Class wasn't found :(");
+//					e.printStackTrace(); //prints the exception message to the console if we can't find the postgresql class
+//				}
+//				
 //				//We need to provide our database credentials
 //				//We'll hardcode them for now, but we'll see how to hide this username/password in environment variables
 //				String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=Krusty_Krab"; **only do it like this if not doing environment variables
 //				String username = "postgres";
 //				String password = "Sparky2014!!"; //this will be whatever password you set in postgres (or RDS if you're using that)
 //											  //hopefully you just left it as password...
-				
-				
-				//I've successfully hidden my DB credentials in my environment variables
-				//run -> run configurations -> environment -> add the environment variable key/pairs you want
-				String url = System.getenv("url");//jdbc:postgresql://localhost:5432/postgres?currentSchema="P1Demo"
-				String username = System.getenv("username");
-				String password = System.getenv("password"); 
-				
-				
-				//This is what returns our actual Connection object (note how this getConnection() method has a return type of Connection
-				return DriverManager.getConnection(url, username, password);
-				
-			}
-			
-		}
+//				
+//				
+//				//I've successfully hidden my DB credentials in my environment variables
+//				//run -> run configurations -> environment -> add the environment variable key/pairs you want
+//				String url = System.getenv("url");//jdbc:postgresql://localhost:5432/postgres?currentSchema="P1Demo"
+//				String username = System.getenv("username");
+//				String password = System.getenv("password"); 
+//				
+//				
+//				//This is what returns our actual Connection object (note how this getConnection() method has a return type of Connection
+//				return DriverManager.getConnection(url, username, password);
+//				
+//			}
+//			
+//		}
